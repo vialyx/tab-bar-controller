fe680b6871e52710616c4d9cd3c1d6f83de75d54
Maxim Vialyx <maksimvialyx@gmail.com>
2018-11-05 18:45:29 +0300

    - init project

---
+++ TabBarController/TabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarController
+//
+//  Created by Maksim Vialykh on 05/11/2018.
+//  Copyright © 2018 Vialyx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
